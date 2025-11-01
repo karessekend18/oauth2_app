461de7f9337e4b5aff6ba2bb31b8028c41fa67f5
Karesse DSouza <karessekend1821@gmail.com>
2025-11-01 21:01:52 +0530

    Initial commit

---
+++ src/test/java/com/oauth/sb_oauth2/SbOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.sb_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
